87a7a2c967a393dae9900b109e40a43c1b35cfb1
Boaz <boaserem022@gmail.com>
2023-07-18 15:24:46 +0300

    added middleware

--- server/controller/postStkController.js
+++ server/controller/postStkController.js
@@ -1,35 +1,11 @@
 const axios = require("axios");
 
-const createToken = async (req, res, next) => {
-  const secret = "crJGlHmGL0ZYA8Zf";
-  const consumer = "AGGz3TU5sJhOZn3P77AuC046WWotmCCv";
-  const auth = new Buffer.from(`${consumer}:${secret}`).toString("base64");
-  await axios
-    .get(
-      "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials",
-      {
-        headers: {
-          authorization: `Basic ${auth}`,
-        },
-      }
-    )
-    .then((data) => {
-      token = data.data.access_token;
-      console.log(data.data);
-      next();
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err.message);
-    });
-};
-
 const postStk = async (req, res) => {
   const shortCode = 174379;
   const phone = req.body.phone.substring(1);
   const amount = req.body.amount;
-  const passkey =    "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
-  
+  const passkey = process.env.passkey;
+
   const url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest";
 
   const date = new Date();
@@ -42,13 +18,13 @@ const postStk = async (req, res) => {
     ("0" + date.getMinutes()).slice(-2) +
     ("0" + date.getSeconds()).slice(-2);
 
-  const password = new Buffer.from(shortCode + passkey + timestamp).toString(    "base64"  );
+  const password = new Buffer.from(shortCode + passkey + timestamp).toString("base64");
 
   const data = {
     BusinessShortCode: shortCode,
     Password: password,
     Timestamp: timestamp,
-    TransactionType: "CustomerPayBillOnline",
+    TransactionType: process.env.TransactionType,
     Amount: amount,
     PartyA: `254${phone}`,
     PartyB: 174379,
@@ -74,4 +50,4 @@ const postStk = async (req, res) => {
     });
 };
 
-module.exports = { createToken, postStk };
+module.exports = {  postStk };
